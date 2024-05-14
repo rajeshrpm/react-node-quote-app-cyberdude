ac334125f7a6a59be7a60a36f7a5f8c7bd5078b0
bharath2023ht <126450752+bharath2023ht@users.noreply.github.com>
2024-05-14 23:43:03 +0530

    update demo2

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="px-4 py-3 bg-rose-500">
-      <h4 className="font-semibold text-xl">Quotes Ready!!</h4>
+      <h4 className="font-semibold text-xl">Quotes Ready!!!</h4>
     </header>
   );
 };
